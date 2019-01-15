2adef5651ffa0bb91c9a19be913ae456533f0cfe
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2019-01-15 10:09:48 +0100

    p

--- Frontend/src/featured/pusher.conf.js
+++ Frontend/src/featured/pusher.conf.js
@@ -1,7 +1,7 @@
 const Pusher = require('pusher-js');
-export const initPusher = (environment="dev") => {
+export const initPusher = (environment="prod") => {
     var env_key = "4f777716babc57b94acb";
-    Pusher.logToConsole = true;
+    Pusher.logToConsole = environment === "dev";
     if (environment === "prod") {
         env_key = "badc896ad3819312d807"
         Pusher.logToConsole = false;

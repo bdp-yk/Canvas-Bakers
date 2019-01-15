051c8049a015df353b910be2c2419fdf65d0f55d
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2019-01-15 10:13:30 +0100

    aa

--- Frontend/src/views/welcome/LoginPage.jsx
+++ Frontend/src/views/welcome/LoginPage.jsx
@@ -21,7 +21,7 @@ class LoginPage extends React.Component {
         super(props);
 
         // reset login status
-        this.props.dispatch(userActions.logout());
+        // this.props.dispatch(userActions.logout());
 
         this.state = {
             user: {

c5ac56249771f0fb512960cfc05dc0d859b9fce7
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2019-01-17 09:14:42 +0100

    include E F

--- backend/modules/app/featured/constants.py
+++ backend/modules/app/featured/constants.py
@@ -1,4 +1,4 @@
-groups_enum = ["A", "B", "C", "D"]
+groups_enum = ["A", "B", "C", "D", "E", "F"]
 classes_enum = ["admin", "tester", "user"]
 verdict_status_enum = ["request", "success", "failure", "existed"]
 canvas_types_enum = ["bmc", "lmc"]

1bd875794edaadb9b70dc0e12de20e6bfd806c00
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2019-02-13 12:45:41 +0100

    http removal

--- backend/wsgi-run.sh
+++ backend/wsgi-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-uwsgi --socket 0.0.0.0:5000 --protocol=https -w wsgi:app
+uwsgi --socket 0.0.0.0:5000 -w wsgi:app
 # uwsgi --http-socket :8787 --home /vol1/MySites/PyTune3/venv --chdir /vol1/MySites/PyTune3 -w wsgi

fd33bcae03c132be67da474589a03de16a7d6606
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2019-02-13 15:26:06 +0100

    aaaa

--- backend/wsgi-run.sh
+++ backend/wsgi-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-uwsgi --ini backend.ini
+# uwsgi --ini backend.ini
+uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app
 # uwsgi --socket 0.0.0.0:5000 -w wsgi:app
 # uwsgi --http-socket :8787 --home /vol1/MySites/PyTune3/venv --chdir /vol1/MySites/PyTune3 -w wsgi
